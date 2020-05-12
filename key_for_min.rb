29c894c186d30c9287f5e1fd0ec25d905d885a9f
Ben Parish <parishben13@gmail.com>
2020-05-12 04:11:53 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -19,28 +19,3 @@ def key_for_min_value(name_hash)
     end
   end
 end
-=begin def key_for_min_value(name_hash)
- min_value = nil
-  min_object = nil
-  name_hash.each do |object, value|
-    if min_value == nil || value < min_value
-      min_value = value
-      min_object = object
-    end
-  end
-  min_object
-end
-=end
-=begin
-shoes= {"cleats": 3, "casual": 4, "tennis":5, "sandals": 2}
-def key_for_min_value(name_hash)
-  array=[]
-name_hash.each do |key, index|
-array << index
-end
-min_num = array[0]
-    array.each do |value|
-      min_num = value if value < min_num
-    end
-end
-=end
